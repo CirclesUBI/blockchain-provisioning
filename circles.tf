a2b09d7b54a207d3d17f4c191512fba8c8fb39f8
David Terry <davidterry@posteo.de>
2018-04-16 13:23:44 +0200

    correct s3 bucket

--- circles.tf
+++ circles.tf
@@ -1,10 +1,7 @@
 terraform {
   backend "s3" {
-    // bucket = "circles-terraform"
-    // region = "us-east-1"
-
-    bucket = "terraform-circles"
-    region = "eu-central-1"
+    bucket = "circles-terraform"
+    region = "us-east-1"
 
     key    = "circles-terraform.tfstate"
     dynamodb_table = "circles-terraform"
