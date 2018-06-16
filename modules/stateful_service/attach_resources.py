403f01f9fb3de20d5c9d02abe87b6ed165848892
David Terry <davidterry@posteo.de>
2018-06-16 04:32:46 +0200

    stateful_service: snapshot volumes before attaching

--- modules/stateful_service/attach_resources.py
+++ modules/stateful_service/attach_resources.py
@@ -11,28 +11,35 @@ import time
 boto3.setup_default_session(region_name="eu-central-1")
 
 
-@retry(exceptions=botocore.exceptions.ClientError, tries=10, delay=1, backoff=2)
-def attach_interface(instance_id, interface_id):
-    ec2 = boto3.resource("ec2")
-    interface = ec2.NetworkInterface(args.interface_id)
-
-    print(
-        f"attach_resources: trying to attach interface {interface_id} to {instance_id}"
+def snapshot_volume(instance_id, volume_id):
+    ec2 = boto3.client("ec2")
+    print(f"attach_resources: snapshotting {volume_id}")
+    snap = ec2.create_snapshot(
+        Description=f"{volume_id}-{instance_id}", VolumeId=volume_id
     )
-    interface.attach(DeviceIndex=1, InstanceId=instance_id)
-    time.sleep(15)  # wait for interface to attach
-    print("attach_resources: interface attached")
+    assert snap["State"] != "error"
 
 
-@retry(exceptions=botocore.exceptions.ClientError, tries=10, delay=1, backoff=2)
+@retry(exceptions=botocore.exceptions.ClientError, tries=5, delay=1, backoff=2)
 def attach_volume(instance_id, volume_id):
     ec2 = boto3.resource("ec2")
     volume = ec2.Volume(args.volume_id)
 
-    print(f"attach_resources: trying to attach volume {volume_id} to {instance_id}")
+    print(f"attach_resources: attaching volume {volume_id} to {instance_id}")
     volume.attach_to_instance(Device="/dev/xvdb", InstanceId=instance_id)
     time.sleep(15)  # wait for volume to attach
     print("attach_resources: volume attached")
+
+
+@retry(exceptions=botocore.exceptions.ClientError, tries=5, delay=1, backoff=2)
+def attach_interface(instance_id, interface_id):
+    ec2 = boto3.resource("ec2")
+    interface = ec2.NetworkInterface(args.interface_id)
+
+    print(f"attach_resources: attaching interface {interface_id} to {instance_id}")
+    interface.attach(DeviceIndex=1, InstanceId=instance_id)
+    time.sleep(15)  # wait for interface to attach
+    print("attach_resources: interface attached")
 
 
 if __name__ == "__main__":
@@ -42,5 +49,6 @@ if __name__ == "__main__":
     parser.add_argument("--volume_id", action="store", required=True)
     args = parser.parse_args()
 
-    attach_interface(args.instance_id, args.interface_id)
+    snapshot_volume(args.instance_id, args.volume_id)
     attach_volume(args.instance_id, args.volume_id)
+    attach_interface(args.instance_id, args.interface_id)
